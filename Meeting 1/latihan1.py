9bd51653ecaaa046c1d146334ec5e8f25605ae38
Ahmad Fikri Haikal <87348156+FikalAhmad@users.noreply.github.com>
2021-10-10 01:04:32 +0700

    Add files via upload

---
+++ Meeting 1/latihan1.py
@@ -0,0 +1,19 @@
+# Struktur Pemprograman Python
+a = 10
+
+if a > 6:
+    print("Lulus")
+else:
+    print("Tidak Lulus")
+
+# Contoh Lain
+x = "Belajar Python"
+y = 851
+phi = 3.14
+
+# Latihan Meeting 1
+print("===========================")
+print("Nama : Sumanto")
+print("Kelas : 12.1A.14")
+print("Jurusan : Sistem Informasi")
+print("===========================")
