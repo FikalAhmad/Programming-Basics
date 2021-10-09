39876a8dba28d4866e8c9a50bbfc79b7876d85e9
Ahmad Fikri Haikal <87348156+FikalAhmad@users.noreply.github.com>
2021-10-10 01:05:04 +0700

    Add files via upload

---
+++ Meeting 2/latihan2.py
@@ -0,0 +1,15 @@
+# Meeting 2
+#Input - Menginput
+print("======== Data Diri Mahasiswa ========")
+nim = input("NIM : ")
+nama = input("Nama Lengkap : ")
+jurusan = input("Jurusan : ")
+alamat = input("Alamat : ")
+
+print("")
+
+print("Hasil Cetak data diatas adalah")
+print("Nim : " +str(nim))
+print("Nama : " +str(nama))
+print("Jurusan : " +str(jurusan))
+print("Alamat : " +str(alamat))
