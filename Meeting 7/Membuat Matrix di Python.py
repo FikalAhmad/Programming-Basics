4f2b93e182f7653a29d6deb71607a68a427772a4
Ahmad Fikri Haikal <87348156+FikalAhmad@users.noreply.github.com>
2021-11-12 23:15:25 +0700

    Add files via upload

---
+++ Meeting 7/Membuat Matrix di Python.py
@@ -0,0 +1,33 @@
+# Membuat Matrix di Python
+
+# Mtriks dengan ukuran 2x2
+matriksA = [[1,0], [0,1]]
+print(matriksA)
+
+# Matriks dengan ukuran 3x3
+matriksB = [[1,0,1], [0,1,0], [1,0,1]]
+print(matriksB)
+
+# Matriks dengan ukuran 4x4
+matriksC = [[1,0,1,0], [0,1,0,1], [1,0,1,0], [0,1,0,1]]
+print(matriksC)
+
+
+
+matrix = [
+    [5, 0],
+    [2, 6],
+]
+
+matrix1 = [
+    [5, 0, 8],
+    [2, 6, 7],
+    [1, 3, 4],
+]
+
+matrix2 = [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+]
